3c4aafdf648372de740e4b0fa371a2679e59f3b6
evg2020 <evg_study@list.ru>
2020-12-10 18:24:51 +0400

    0.1. FileOutputStream. [#391411]

--- src/main/java/chapter_001/io/MultTable.java
+++ src/main/java/chapter_001/io/MultTable.java
@@ -12,8 +12,6 @@ public class MultTable {
                 System.out.println( multTabl);
             }*/
         String multTabl = "1*1 =1, 1*2=2, 1*3=3, 1*4=4, 1*5=5, 1*6=6, 1*7=7";
-                ;
-
         try (FileOutputStream out = new FileOutputStream("MultTable.txt")) {
             out.write(multTabl.getBytes());
         } catch (Exception e) {
