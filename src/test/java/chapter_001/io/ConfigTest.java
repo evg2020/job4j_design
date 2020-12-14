53d80279ff8c404f3ab210d0f8c7fa7c35049e50
evg2020 <evg_study@list.ru>
2020-12-14 16:21:21 +0400

    1. Читаем файл конфигурации [#391417]

--- src/test/java/chapter_001/io/ConfigTest.java
+++ src/test/java/chapter_001/io/ConfigTest.java
@@ -9,7 +9,7 @@ public class ConfigTest {
 
     @Test
     public void whenPairWithoutComment() {
-        String path = "./data/pair_without_comment.properties";
+        String path = ".pair_without_comment.properties";
         Config config = new Config(path);
         config.load();
         assertThat(
