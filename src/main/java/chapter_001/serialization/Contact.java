7db7566c24bc6180e200027f3468c9593a2dabfd
evg2020 <evg_study@list.ru>
2021-01-16 13:20:44 +0400

    1. Что такое Сериализация? [#391423]

--- src/main/java/chapter_001/serialization/Contact.java
+++ src/main/java/chapter_001/serialization/Contact.java
@@ -35,15 +35,13 @@ public class Contact implements Serializable {
         /* Запись объекта в файл */
         File tempFile = Files.createTempFile(null, null).toFile();
         try (FileOutputStream fos = new FileOutputStream(tempFile);
-             ObjectOutputStream oos =
-                     new ObjectOutputStream(fos)) {
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(contact);
         }
 
         /* Чтение объекта из файла */
         try (FileInputStream fis = new FileInputStream(tempFile);
-             ObjectInputStream ois =
-                     new ObjectInputStream(fis)) {
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
             final Contact contactFromFile = (Contact) ois.readObject();
             System.out.println(contactFromFile);
         }
